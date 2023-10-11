518ac3f8d8f7767c3e0a80f9d9eb212e6925a2f6
Saad el kelkha <146880449+Saadelkelkha@users.noreply.github.com>
2023-10-11 09:36:34 +0100

    Add files via upload

---
+++ ten number do while ex22.py
@@ -0,0 +1,7 @@
+n=int(input("type the number :"))
+i=n+1
+while True:
+    print(i)
+    i=i+1
+    if i==n+11 :
+        break
---
+++ ten number ex22 while.py
@@ -0,0 +1,5 @@
+n=int(input("type the number :"))
+i=n+1
+while i<=n+10 :
+    print(i)
+    i=i+1
